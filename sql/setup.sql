87e17bef112117bcd873d231bca145a0a2e08457
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-03 13:12:16 -0700

    created cars-data and setup sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS cars;
+
+CREATE TABLE cars (
+    id BIGINT GENERATED ALWAYS AS IDENTITY
+    make VARCHAR NOT NULL,
+    model VARCHAR NOT NULL,
+    year INT NOT NULL,
+);
+
+INSERT INTO cars (make, model, year)
+VALUES
+('toyota', 'corolla', 1996),
+('Lexus', 'IS500', 2022),
+('BMW', 'M5', 2018),
+('Audi', 'RS7', 2020),
+('Chevorlet Corvette','ZR1', 2019)
