6407bf4dfda09504d2d9c7a65469e66589b1d58e
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-04 17:10:28 -0700

    built cars full list test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,7 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
+const { cars } = require('../lib/cars-data.js');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
@@ -7,8 +9,12 @@ describe('cars routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/cars should return list of cars', async () => {
+    const res = await request(app).get('/cars');
+    const expected = cars.map((car) => {
+      return { id: car.id, name: car.make, model: car.model, year: car.year };
+    });
+    expect(res.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
